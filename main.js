4f7ba252586add709d9f01dfeded18b006539a65
GiacomoGaudino <giacomo.gaudino@yahoo.it>
2025-07-25 19:39:58 +0200

    add soccer array

--- main.js
+++ main.js
@@ -66,17 +66,6 @@ const lightestWeight = getMinWeight(bikes);
 console.log(`il peso minimo è ${lightestWeight}kg`);
 
 
-
-
-
-
-
-
-
-
-
-
-
 /*
 Snack2
 Creare un array di oggetti di squadre di calcio.
@@ -85,3 +74,67 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
+
+const soccerTeams = [
+    {
+        id: 1,
+        nome: "Inter",
+        falliSubiti: 0,
+        puntiFatti: 0
+    },
+    {
+        id: 2,
+        nome: "Milan",
+        falliSubiti: 0,
+        puntiFatti: 0
+    },
+    {
+        id: 3,
+        nome: "Napoli",
+        falliSubiti: 0,
+        puntiFatti: 0
+    },
+    {
+        id: 4,
+        nome: "Roma",
+        falliSubiti: 0,
+        puntiFatti: 0
+    },
+    {
+        id: 5,
+        nome: "Lazio",
+        falliSubiti: 0,
+        puntiFatti: 0
+    },
+    {
+        id: 6,
+        nome: "Atalanta",
+        falliSubiti: 0,
+        puntiFatti: 0
+    },
+    {
+        id: 7,
+        nome: "Fiorentina",
+        falliSubiti: 0,
+        puntiFatti: 0
+    },
+    {
+        id: 8,
+        nome: "Torino",
+        falliSubiti: 0,
+        puntiFatti: 0
+    },
+    {
+        id: 9,
+        nome: "Sassuolo",
+        falliSubiti: 0,
+        puntiFatti: 0
+    },
+    {
+        id: 10,
+        nome: "Empoli",
+        falliSubiti: 0,
+        puntiFatti: 0
+    }
+];
+console.log(soccerTeams);
